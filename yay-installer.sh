39eebfb2d082b896e181d3a87f25d25b0571e33e
Caleb Bassi <calebjbassi@gmail.com>
2018-03-31 13:27:08 -0700

    Initial commit

---
+++ yay-installer.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# Make sure our shiny new arch is up-to-date
+echo "Checking for system updates..."
+sudo pacman -Syu
+
+# Create a tmp-working-dir and navigate into it
+mkdir -p /tmp/yay_install
+cd /tmp/yay_install
+
+# # If you didn't install the "base-devel" group,
+# # we'll need those.
+# sudo pacman -S binutils make gcc fakeroot pkg-config --noconfirm --needed
+
+# # Install yay dependencies from arch repos
+# sudo pacman -S expac yajl git --noconfirm --needed
+
+# Install "yay" from AUR
+if [ ! -n "$(pacman -Qs yay)" ]; then
+    curl -o PKGBUILD https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=yay
+    makepkg PKGBUILD --install --needed --noconfirm
+fi
+
+# Clean up...
+cd ~
+rm -r /tmp/yay_install
